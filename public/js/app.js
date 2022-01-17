4730441ddcd5fd67d5712f9d560576c7268ba59e
BlueCrafter6 <yukale.moodliar@gmail.com>
2022-01-17 11:11:10 +0200

    BIG COMMIT

---
+++ public/js/app.js
@@ -0,0 +1,63 @@
+const doc = document;
+var profanity = [
+    "ass", "shit", "fuck", "dick", "bitch"
+]
+var censor = ("*******")
+  ;
+
+profanity = new RegExp(profanity.join("|") ,"gi");
+
+function clean(s){
+    return s.replace(profanity
+                    , function(m) { 
+                         return censor.substring(0, m.length)
+                      } 
+                    );
+}
+
+//________________________________________________________
+
+const userNameInput = doc.getElementById('userNameInput');
+const signInDiv = doc.getElementById('username');
+const signInNav = doc.getElementById('signin');
+const signOutNav = doc.getElementById('signout');
+const errorDiv = doc.getElementById('error');
+const errp = doc.getElementById('errp');
+  
+function showSignInField() {
+    signInDiv.style.display = 'block';
+}
+
+function signIn() {
+    if (userNameInput.value == '') { 
+        errp.textContent = "You have not entered anything as your username"
+        errorDiv.style.display = 'block'
+        signInDiv.style.display = 'none'
+    return
+    }
+
+    if (userNameInput.value.length < 8) {
+        errp.textContent = "Your username is under 8 characters"
+        errorDiv.style.display = 'block'
+        signInDiv.style.display = 'none'
+        return
+    }
+
+    signInNav.textContent = clean(userNameInput.value);
+    alert(`Signed in!\n\nClick on your username (${clean(userNameInput.value)}) to change it`)
+    userNameInput.value = '';
+    signInDiv.style.display = 'none';
+    signOutNav.style.display = 'block';
+    errorDiv.style.display = 'none'
+}
+
+function signOut() {
+    signOutNav.style.display = 'none'
+    signInNav.textContent = 'Sign In'
+}
+
+function closeError() {
+    errorDiv.style.display = 'none'
+}
+
+//___________________________________________________
